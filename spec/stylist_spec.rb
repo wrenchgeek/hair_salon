c9fd2d459b4517b9179b38fce97f0e2dbeec94c7
Jim Patt <wrench1138@gmail.com>
2015-05-08 12:17:49 -0700

    add and pass spec to delete a stylist from the database

--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -46,4 +46,13 @@ describe(Stylist) do
     end
   end
 
+  describe('#delete') do
+    it('deletes a stylist from the database') do
+      test_stylist = Stylist.new({:name => "Fritz", :id => nil})
+      test_stylist.save()
+      test_stylist.delete()
+      expect(Stylist.all()).to(eq([]))
+    end
+  end
+
 end
