898a1a313b3524b2348119869ae882c379397368
Jim Patt <wrench1138@gmail.com>
2015-05-08 11:49:41 -0700

    add and pass spec to save stylist to database

--- lib/stylists.rb
+++ lib/stylists.rb
@@ -8,14 +8,24 @@ class Stylist
   end
 
   define_singleton_method(:all) do
-  returned_stylists = DB.exec("SELECT * FROM stylists;")
-  stylists = []
-  returned_stylists.each() do |stylist|
-    name = stylist.fetch("name")
-    id = stylist.fetch('id')
-    stylists.push(Stylist.new({:name => name, :id => id,}))
+    returned_stylists = DB.exec("SELECT * FROM stylists;")
+    stylists = []
+    returned_stylists.each() do |stylist|
+      name = stylist.fetch("name")
+      id = stylist.fetch('id')
+      stylists.push(Stylist.new({:name => name, :id => id,}))
+    end
+    stylists
   end
-  stylists
+
+  define_method(:save) do
+    result = DB.exec("INSERT INTO stylists (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
+
+  define_method(:==) do |another_stylist|
+  self.name().==(another_stylist.name()).&(self.id().to_i().==(another_stylist.id().to_i()))
 end
+
 
 end
--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -16,9 +16,17 @@ describe(Stylist) do
   end
 
   describe('.all') do
-  it('returns all of the stylists') do
-  expect(Stylist.all()).to(eq([]))
+    it('returns all of the stylists') do
+    expect(Stylist.all()).to(eq([]))
+    end
   end
-end
+
+  describe('#save') do
+    it('saves the stylist to the database') do
+    test_stylist = Stylist.new({:name => "Fabio", :id => nil})
+    test_stylist.save()
+    expect(Stylist.all()).to(eq([test_stylist]))
+    end
+  end
 
 end
